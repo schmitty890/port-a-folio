778f82d0818313d13dda8a10e0b53d2c8385f255
jschmitt <schmitty890@gmail.com>
2019-02-19 16:52:33 -0500

    adding wrapper function for debugging hosting

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 /**
  * Module dependencies.
  */
