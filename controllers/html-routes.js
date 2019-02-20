b78b7471c02eb9a9b65c0c358eb6880939ad84b9
jschmitt <schmitty890@gmail.com>
2019-02-19 20:54:11 -0500

    removing extra json

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,10 +1,3 @@
-var indexData = require('../data/indexData.json');
-var gettingStartedData = require('../data/gettingStartedData.json');
-var guidelinesData = require('../data/guidelinesData.json');
-// var stylesData = require('../data/stylesData.json');
-var componentsData = require('../data/componentsData');
-var navigation = require('../data/navigation.json');
-var releaseNotesData = require('../data/releaseNotesData.json');
 var jobs = require('../data/jobs.json');
 
 module.exports = function (app) {
@@ -12,9 +5,7 @@ module.exports = function (app) {
   app.get('/', function (req, res) {
     // assign the handlebar object any data to be read into the template. this separates the data from the markup.
     var hbsObject = {
-      pageData: indexData,
-      headerData: componentsData,
-      nav: navigation
+      jobs: jobs
     };
     console.log(hbsObject);
     // console.log(hbsObject);
@@ -27,8 +18,5 @@ module.exports = function (app) {
   app.get('/api/jobs', function(req, res) {
     res.send(jobs);
   });
-  
-
- 
 
 };
