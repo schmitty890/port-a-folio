13a2027951e0ee17c7cfbcef0a755c6228a19e53
jschmitt <schmitty890@gmail.com>
2019-02-19 20:05:27 -0500

    exposing jobs

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -5,6 +5,7 @@ var guidelinesData = require('../data/guidelinesData.json');
 var componentsData = require('../data/componentsData');
 var navigation = require('../data/navigation.json');
 var releaseNotesData = require('../data/releaseNotesData.json');
+var jobs = require('../data/jobs.json');
 
 module.exports = function (app) {
   // Home Page
@@ -23,44 +24,11 @@ module.exports = function (app) {
     });
   });
 
-  // Getting Started Page
-  app.get('/getting-started', function (req, res) {
-    // assign the handlebar object any data to be read into the template. this separates the data from the markup.
-    var hbsObject = {
-      pageData: gettingStartedData,
-      nav: navigation
-    };
-    // console.log(hbsObject);
-    res.render('partials/pages/getting-started', {
-      title: 'Home', // pass any value to handlebar template
-      hbsObject: hbsObject
-    });
+  app.get('/api/jobs', function(req, res) {
+    res.send(jobs);
   });
+  
 
-  // Components Pages
-  app.get('/components/:category', function(req, res) {
-    var category = req.params.category;
-    var hbsObject = {
-      pageData: componentsData.components[category],
-      nav: navigation
-    };
+ 
 
-    res.render('pages/components', {
-      hbsObject: hbsObject
-    });
-  });
-
-  // Release Notes
-  app.get('/release-notes', function(req, res) {
-    var hbsObject = {
-      pageData: releaseNotesData,
-      nav: navigation
-    };
-
-    console.log(releaseNotesData);
-
-    res.render('pages/release-notes', {
-      hbsObject: hbsObject
-    });
-  });
 };
