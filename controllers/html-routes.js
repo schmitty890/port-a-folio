91ad881dcc4e800ccf0ee245e2631d72ef837397
jschmitt <schmitty890@gmail.com>
2019-02-28 19:42:27 -0500

    calendar api testing

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -100,7 +100,7 @@ module.exports = function (app) {
     const fs = require('fs');
 
     // If modifying these scopes, delete token.json.
-    const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+    const SCOPES = ['https://www.googleapis.com/auth/calendar'];
     // The file token.json stores the user's access and refresh tokens, and is
     // created automatically when the authorization flow completes for the first
     // time.
@@ -179,6 +179,52 @@ module.exports = function (app) {
         if (err) return console.log('The API returned an error: ' + err);
         const events = resp.data.items;
         var hbsObject = resp.data.items;
+//////////////////////////////////////
+// Refer to the Node.js quickstart on how to setup the environment:
+// https://developers.google.com/calendar/quickstart/node
+// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+// stored credentials.
+
+var event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2019-03-12T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2019-03-15T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'},
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+};
+
+calendar.events.insert({
+  auth: auth,
+  calendarId: 'primary',
+  resource: event,
+}, function(err, event) {
+  if (err) {
+    console.log('There was an error contacting the Calendar service: ' + err);
+    return;
+  }
+  console.log('Event created: %s', event.htmlLink);
+});
+//////////////////////////////////////
         res.send(resp.data.items);
         // res.render('googleapi', {
         //   hbsObject: hbsObject
