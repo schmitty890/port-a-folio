9e9ba42869d23da098942ba6073f9816409bedeb
jschmitt <schmitty890@gmail.com>
2019-03-02 17:23:16 -0500

    accepting form field values and posting to google api

--- public/assets/js/custom/googleapi.js
+++ public/assets/js/custom/googleapi.js
@@ -11,38 +11,7 @@ var GoogleAPI = function(){
     });
   };
 
-  function postEvent() {
-
-    // @TODO: get user input, pass it to post /api/google-api
-    var event = {
-      'summary': 'The main title',
-      'location': '1416 enchanted oaks dr',
-      'description': 'a lovely description',
-      'start': {
-        'dateTime': '2019-03-13T21:10:00',
-        'timeZone': 'America/New_York',
-      },
-      'end': {
-        'dateTime': '2019-03-15T21:00:00',
-        'timeZone': 'America/New_York',
-      },
-      'attendees': [
-        {'email': 'schmitty890@gmail.com',
-          'additionalGuests': 1,
-          'comment': 'the guests personal comment',
-          'displayName': 'Jason'
-        },
-        {'email': 'anotherperson@example.com'},
-      ],
-      'reminders': {
-        'useDefault': false,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10},
-        ],
-      }
-    };
-    console.log(event);
+  function postEvent(event) {
     $.ajax("/api/google-api", {
       type: "POST",
       data: event
@@ -58,8 +27,51 @@ var GoogleAPI = function(){
       getCalendar();
     });
     $(document).on('click', '#post-event', function() {
-      console.log('post-event clicked!');
-      postEvent();
+      var summary = $('.summary').val();
+      var startTime = $('.start-date').val();
+      var endTime = $('.end-date').val();
+
+
+      startTime = convertTimeRealQuick(startTime);
+      endTime = convertTimeRealQuick(endTime);
+
+      // regex add a T at space, add :00 at end
+      function convertTimeRealQuick(time) {
+        time = time.replace(/ /g,"T");
+        time += ':00';
+        return time;
+      }
+
+      // create payload to send to server
+      var event = {
+        'summary': summary,
+        'location': $('.location').val(),
+        'description': $('.description').val(),
+        'start': {
+          'dateTime': startTime,
+          'timeZone': 'America/New_York',
+        },
+        'end': {
+          'dateTime': endTime,
+          'timeZone': 'America/New_York',
+        },
+        'attendees': [
+          {'email': $('.attendee-email').val(),
+            'additionalGuests': $('.attendee-guests').val(),
+            'comment': $('.attendee-comment').val()
+          },
+          {'email': 'anotherperson@example.com'},
+        ],
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+          ],
+        }
+      };
+      // send event to server
+      postEvent(event);
     });
   }
 
@@ -83,3 +95,30 @@ GoogleAPI.init();
 
 
 
+var Form = function(){
+
+  /**
+   * [initDate. Our date fields should have an ID of #date. Any field with this id, will have the flatpickr calendar enabled on it
+   * NOTE: There should not be more than one of the same ID on a form. if we need mulitple dates, create another id and add that handler here]
+   */
+  function initDate() {
+    $(".start-date, .end-date").flatpickr({
+      enableTime: true,
+      dateFormat: "Y-m-d H:i"
+    });
+  }
+
+  /**
+   * [init. list of all the functions we run when we initalize the module.]
+   */
+  function init() {
+    initDate();
+  }
+
+  return {
+    init: init
+  }
+
+}();
+
+Form.init();
