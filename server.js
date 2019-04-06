af4e6cd3554af827d4791bb1111cadedf38a05a8
jschmitt <schmitty890@gmail.com>
2019-04-06 16:10:53 -0400

    adding passport and mongodb database to portfolio

--- server.js
+++ server.js
@@ -7,13 +7,75 @@ const chalk = require('chalk');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const fs = require('fs');
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
+const flash = require('express-flash');
+const lusca = require('lusca');
+const expressValidator = require('express-validator');
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: '.env' });
+
+// require models
+var db = require("./models");
 
 
 /**
  * Create Express server.
  */
 const app = express();
+
+/**
+ * Connect to MongoDB.
+ */
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/thejasonschmitt';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+app.use(expressValidator());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'anything',
+  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+  store: new MongoStore({
+    url: MONGODB_URI,
+    autoReconnect: true,
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+app.disable('x-powered-by');
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+app.use((req, res, next) => {
+  // After successful login, redirect back to the intended page
+  if (!req.user &&
+    req.path !== '/login' &&
+    req.path !== '/signup' &&
+    !req.path.match(/^\/auth/) &&
+    !req.path.match(/\./)) {
+    req.session.returnTo = req.originalUrl;
+  } else if (req.user &&
+    (req.path === '/account' || req.path.match(/^\/api/))) {
+    req.session.returnTo = req.originalUrl;
+  }
+  next();
+});
 
 
 /**
@@ -50,6 +112,7 @@ app.use('/public', express.static(__dirname + '/public'));
  * Get all routes
  */
 require('./controllers/html-routes.js')(app);
+require('./controllers/auth-routes.js')(app);
 
 /**
  * show 404 page if no route has been hit
